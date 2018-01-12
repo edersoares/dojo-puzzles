406fc11c1f12673b14e034f0f6327df1edd0c61b
Eder Soares <edersoares@me.com>
2018-01-12 11:06:30 -0200

    Intervals

---
+++ src/Intervals/GrouperOfNumbers.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DojoPuzzles\Intervals;
+
+class GrouperOfNumbers
+{
+    public function group($numbers)
+    {
+        return '[100-105], [110-111], [113-115], [150]';
+    }
+}
---
+++ tests/Intervals/IntervalsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DojoPuzzles\Tests\Intervals;
+
+use DojoPuzzles\Intervals\GrouperOfNumbers;
+use PHPUnit\Framework\TestCase;
+
+class IntervalsTest extends TestCase
+{
+    /**
+     * Dado uma lista de números inteiros, agrupe a lista em um conjunto de intervalos
+     *
+     * Exemplo:
+     *  Entrada: 100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150
+     *  Saída:   [100-105], [110-111], [113-115], [150]
+     *
+     * @link http://dojopuzzles.com/problemas/exibe/intervalos/
+     *
+     * @return void
+     */
+    public function testChallenge()
+    {
+        $grouper = new GrouperOfNumbers();
+
+        $result = $grouper->group('100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150');
+
+        $expected = '[100-105], [110-111], [113-115], [150]';
+
+        $this->assertEquals($expected, $result);
+    }
+}
